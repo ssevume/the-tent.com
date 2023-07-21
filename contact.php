95b72b37c4f0feda95a2a60165f51a6d2d2974cf
ssevume <140143705+ssevume@users.noreply.github.com>
2023-07-21 09:26:10 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require_once'lawrence.php';
+if(isset($_POST["submit"])){
+    $topic = mysqli_escape_string($law, $ POST["topic"]);
+    $message = mysqli_escape_string($law, $_POST)
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+    <form action="#" method="POST">
+        <label for="topic">Topic</label
+
+</body>
+</html>
